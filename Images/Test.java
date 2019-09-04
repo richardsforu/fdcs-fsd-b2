5ea2541214b575b007575c35337bae35c50e6bd2
ctsjava <ctsgit@gmail.com>
2019-09-04 18:39:39 +0530

    day3

---
+++ Images/Test.java
@@ -0,0 +1,29 @@
+class Test{
+
+
+public static void main(String[] blabla){
+
+  int x=10;
+  int y=20;
+
+ System.out.println(x);
+ System.out.println("X= "+x);
+  System.out.println(+x+x);
+ System.out.println(" "+x+" "+x+" "+(x+x));
+ System.out.println("Sum of "+x+" and "+y+" is "+(x+y));
+
+ int i=1234567;
+ System.out.println(i);
+
+  java.util.Scanner sc=new java.util.Scanner(System.in);
+  System.out.println("Enter a Number: ");
+  int n=sc.nextInt();
+  System.out.println("Number is: "+n);
+
+
+}	
+}
+
+class C2{}
+
+
