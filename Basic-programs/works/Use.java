4a037ed2edc3ee2e070f98f58a939ce4e75ea7f7
ctsjava <ctsgit@gmail.com>
2019-09-11 17:27:40 +0530

    Day8

---
+++ Basic-programs/works/Use.java
@@ -0,0 +1,18 @@
+import arth.Sum;
+import arth.Prod;
+import arth.Div;
+class Use{
+
+public static void main(String[] aa){
+Sum s=new Sum();
+Prod p=new Prod();
+Div d=new Div();
+
+  s.sum(100,200);
+  p.prod(15,2);
+  d.div(100,2);
+
+ }
+
+
+}
---
+++ Java SE/Encapulation-Demo/src/com/Notes.java
@@ -0,0 +1,41 @@
+package com;
+
+import java.sql.Date;
+
+public class Notes {
+	
+
+	
+	/*
+	 * 			Encapulasion
+	 * 
+	 *    -> Wrapping of methods and properties together
+	 *    
+	 * 	   
+	 *   1. packages
+	 *   		-> built-in
+	 *   		-> user defined
+	 *   2. access modifiers
+	 *   		-> default
+	 *   		-> public
+	 *   		-> protected
+	 *   		-> private
+	 *   --------------------------------------
+	 *   
+	 *   
+	 *   POJO/Bean/Model/Entity class (Plain old java object)
+	 *   
+	 *   
+	 *   
+	 *    1. packages
+	 *    ----------------
+	 *    
+	 *    -> Packing up  group of related classes into a specified folder
+	 *    Note: package keyword must be the first statement in the class
+	 *    
+	 *   
+	 *   
+	 * 
+	 */
+
+}
