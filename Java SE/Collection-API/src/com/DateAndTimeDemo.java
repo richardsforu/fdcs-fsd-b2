59d5415efb19772b53efd77f31efb4245154e23d
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2019-10-31 16:42:00 +0530

    Case study solutions

--- Java SE/Collection-API/src/com/DateAndTimeDemo.java
+++ Java SE/Collection-API/src/com/DateAndTimeDemo.java
@@ -11,11 +11,15 @@ public class DateAndTimeDemo {
 		
 		System.out.println(LocalDate.of(1987, 10, 12));
 
-		String dob="1981-07-19";
+		String dob="1981-10-25";
 		
 		Period p=Period.between( LocalDate.parse(dob),ld);
 		
 		System.out.println(p.getYears());
+		System.out.println(p.getMonths());
+		System.out.println(p.getDays());
+		
+		
 		
 
 	}
